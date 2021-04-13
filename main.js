a1062ca38595e73d9a45f171e6a90a318234d4cc
Jorge Mario <you@example.com>
2021-04-12 20:28:53 -0500

    MI PRIMER COMMIT # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit.

---
+++ main.js
@@ -0,0 +1,6 @@
+document.querySelector('.menu-btn').addEventListener('click',() =>{document.querySelector('.nav-menu').classList.toggle('show');});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-card',{delay: 500});
+ScrollReveal().reveal('.tarjeta_banner_one',{delay: 500});
+ScrollReveal().reveal('.tarjeta_banner_two',{delay: 500});
